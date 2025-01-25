790605acf7d10a76876d421eb6fc10dcc689d784
nguyen-tho <nguyencongtho116@gmail.com>
2025-01-25 07:52:50 +0700

    update preprocess and visualize

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,11 @@
 import read_data as r
-
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from imblearn.over_sampling import SMOTE
+from collections import Counter
+import numpy as np
+"""
 data_path = '../data/vd.csv'
-test_path = '../data/new_test_data.csv'
+test_path = '../data/new_test.csv'
 
 #read train and test set
 data = r.read_data(data_path)
@@ -10,7 +14,7 @@ test_data = r.read_data(test_path)
 #set datetime as index
 data = r.set_date_as_index(data)
 test_data = r.set_date_as_index(test_data)
-
+"""
 def data_mapping(data):
     classes = {
         'Không mưa':0,
@@ -33,8 +37,8 @@ def data_mapping(data):
     
     return data
 
-data = data_mapping(data)
-test_data = data_mapping(test_data)
+#data = data_mapping(data)
+#test_data = data_mapping(test_data)
 
 def feature_and_target(data):
     #features
@@ -43,9 +47,40 @@ def feature_and_target(data):
     target = data.rain_status
     return features, target
 
-feature, target = feature_and_target(data)
-test_feature, test_target = feature_and_target(test_data)
+#feature, target = feature_and_target(data)
+#test_feature, test_target = feature_and_target(test_data)
     
+def data_normalization(X):
+    #normalize data with min-max scaling for features X
+    scaler = MinMaxScaler(feature_range=(0,1))
+    X = scaler.fit_transform(X)
+    X = X.reshape(X.shape[0], 1, X.shape[1]) #reshape to 3D array for LSTM 
+    
+    return X
 
+def target_encoding(y):
+    #one-hot encoding for target y
+    encoder = OneHotEncoder()
+    y_encoded = encoder.fit_transform(y.reshape(-1, 1))
+    y_encoded = y_encoded.toarray()
+    
+    return y
+
+def over_sampling(X, y):
+    #oversampling the minority class in y
+    smote = SMOTE(random_state=42)
+    X_res, y_res = smote.fit_resample(X, y)
+    return X_res, y_res
+
+def over_sampling_for_valid_set(X_val, Y_val):
+    # Calculate k_neighbors based on the minimum number of samples in each class in Y_val
+    minority_class_counts = [count for _, count in Counter(np.argmax(Y_val, axis=1)).items() if count <= 5]
+    k_neighbors_val = min(5, min(minority_class_counts) - 1) if minority_class_counts else 1  # Ensure k_neighbors_val is at least 1
+
+    # Adjust k_neighbors for validation set based on the count of the rarest class
+    smote_val = SMOTE(random_state=42, k_neighbors=k_neighbors_val)
+    X_val, Y_val = smote_val.fit_resample(X_val, Y_val)
+    
+    return X_val, Y_val
     
 
--- src/visualize.py
+++ src/visualize.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def pie_plot(data_col, autopct='%1.1f%%'):
     data_values = data_col.value_counts().values
@@ -6,5 +7,25 @@ def pie_plot(data_col, autopct='%1.1f%%'):
     
     plt.pie(data_values, labels=labels, autopct=autopct)
     plt.show()
+    
+    
+def scatter_3D_plot(data, group, col1, col2, col3, title):
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    groups = data.groupby(group)
+    
+    for name, group in groups:
+        ax.scatter(group[col1], group[col2], group[col3], label=name)
+        
+    ax.set_xlabel(col1)
+    ax.set_ylabel(col2)
+    ax.set_zlabel(col3)
+    ax.set_title(title)
+    
+    ax.legend()
+    
+    plt.show()
+    
     
     
